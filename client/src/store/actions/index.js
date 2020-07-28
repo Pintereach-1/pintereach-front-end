7e8f5d127c4e9f7124377cf905eec1e617e87b11
Jorge Manzur <46169161+manzur1990@users.noreply.github.com>
2020-07-28 10:48:33 -0500

    Update index.js

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,10 +1,1 @@
-import axios from "axios";
 
-export const AxiosWithAuth = () => {
-  const token = window.localStorage.getItem("token");
-  return axios.create({
-    headers: {
-      authorization: token
-    }
-  });
-};
