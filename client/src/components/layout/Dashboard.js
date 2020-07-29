8fef8193d3937e2458f2a6cdfc0f9dd5f10b132d
JM <manzur-1990@hotmail.com>
2020-07-29 09:18:22 -0500

    connected  navbar and boardform to dashbar components

--- client/src/components/layout/Dashboard.js
+++ client/src/components/layout/Dashboard.js
@@ -1,15 +1,65 @@
-import React from 'react'
+import React, { useEffect } from "react";
+// import {Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-export const Dashboard = () => {
+import { NavBar } from "./NavBar";
+import { BoardForm } from "../board/BoardForm";
+
+
+export const Dashboard = () =>{
+  
+  
+  useEffect(() => {
+    // const user_id = localStorage.getItem("user id");
+    //getBoards(user_id)
+  }, [])
+  
+    
+  //deleteBoard handle
+
+   
     return (
-        <div>
-            <ul>
-                <li>Yolo</li>
-                <li>Yolo</li>
-                <li>Yolo</li>
-                <li>Yolo</li>
-                <li>Yolo</li>
-            </ul>
+      <div className="">
+        <NavBar />
+        <div className="">
+          <BoardForm />
         </div>
-    )
-}
+    
+        {/* <div className="">
+          {boards.map(board => {
+            return (
+              <div className="" key={board.id}>
+                <Link
+                  className=""
+                  to={`/dashboard/${board.id}`}
+                  key={board.id}
+                >
+                  <i className="" id="" />
+                  <h1>{board.board_title}</h1>
+                </Link>
+                <button
+                  className=""
+                  to="/dashboard"
+                //   onClick={() => deleteBoard(board.id)}
+                >
+                  Delete Board
+                </button>
+              </div>
+            );
+          })}
+        </div> */}
+      </div>
+    );
+  }
+
+
+const mapStateToProps = state => ({
+  boards: state.boards,
+});
+
+console.log();
+
+export default connect(
+  mapStateToProps,
+//   { getBoards, deleteBoard, editBoard }
+)(Dashboard);
--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
     return (
-        <div>
-            
+        <div className="">
+            <div className="">
+                {" "}
+                <i className="" />
+                <div className="">
+                    <h1>Pintereach</h1>
+                </div>
+            </div>
+
+            <div>
+                <NavLink className="" to="/dashboard">
+                    Dashboard
+                </NavLink>
+                <NavLink className="" to="/user">
+                    Profile
+                </NavLink>
+                <NavLink className="" to="/login">
+                    Logout
+                </NavLink>
+
+            </div>
         </div>
     )
 }
